3f196d23808986685387e588d5219eea944e23af
Joseph Leskey <josleskey@mail.mvnu.edu>
2025-01-15 04:37:08 -0500

    Improve sqrt program

--- program2/sqrt.c
+++ program2/sqrt.c
@@ -1,15 +1,29 @@
-// Author: Joseph Leskey
-// Date: 14 January 2024
+/**
+ * @file sqrt.c
+ * @author Joseph Leskey
+ * @date 15 January 2024
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
 int main(int argc, char const *argv[])
 {
     float input;
+
     printf("Enter a real number: ");
-    scanf("%f", &input);
-    printf("\n");
-    printf("The square root of %f is %f\n", input, sqrtf(input));
+
+    if (scanf("%f", &input) != 1) {
+        fprintf(stderr, "Bad input.\n");
+        return EXIT_FAILURE;
+    }
+
+    float result = input < 0 ? sqrtf(-input) : sqrtf(input);
+    char notation = input < 0 ? 'i' : '\0';
+
+    printf("\nThe square root of %f is %f%c\n", input, result, notation);
+
+    return EXIT_SUCCESS;
 }
